43530ed81d7c7e63379787c9394df430ec73129a
Jackson Codd <coddjackson@gmail.com>
2025-07-15 10:07:56 -0400

    Very abstract but I'm excited to implement it

--- personality.py
+++ personality.py
@@ -19,4 +19,14 @@ Then, as we continue to talk, the other memories spread out from here like tree 
 
 The thing with this is it needs to be fluid. The mind is always changing and we need to account for that by being able to add new memories and remove old one and even modify ones if they are referenced often. You will remember something better if people are constantly talking about it. This will make those memories grow and become more important. Conversely, if you never talk about something, it will shrink, but never removed completely, smoe details might be removed but the memory will be there (it might fade as it does in humans).
 
+Emotional Weight: Some memories will have a higher growth rate than others based on emotional weight. For example, a traumatic event might grow rapidly in size and importance and shrink very slowly, while a mundane event might shrink over time.
+
+Color: Some memories will have different colors to represent their nature or significance. For example, a happy memory might be bright yellow, while a sad memory might be a darker blue. This color coding can help in quickly identifying the emotional context of a memory.
+
+State based memory: Humans remember things with associations so if I am studying late at night, I will remember it better late at night than in the morning. This can be represented by associating memories with certain states or contexts, such as time of day, location, or emotional state.
+
+Unconscious Influences (This one I will need to be careful with): Some memories could influence behavior without being directly accessible. Like if someone was rude when discussing a topic, future conversations about that topic might be slightly more cautious, even if Hozie doesn't explicitly remember why.
+
+Dream mechanics: Maybe during a sleeping period, memories can reogranize or create new connections between each other. This could be a way to strengthen certain memories or create new associations, similar to how dreams can help process information and emotions in humans.
+
 '''
